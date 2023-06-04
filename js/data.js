cca3cf4fb302142d637dd40268fa6227aaabf033
Евгений Симанин <130032501+EvgeniiSim@users.noreply.github.com>
2023-06-04 11:40:48 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ const cardsData = [
         producer: 'АиР Все',
         img: 'images/knifes/one/nozh-lesnoy-110h18m-shd-kompozit-soty-alyuminiy-foto-01-min.jpg',
         size: 320,
-        source: 'product-1.html'
+        source:'product-1.html'
     },
     {
         id:2,
@@ -17,7 +17,7 @@ const cardsData = [
         producer: 'ЗИК Все',
         img: 'images/knifes/two/nozh-kunica-2-110h18m-shd-stabilizirovannaya-drevesina-alyuminiy-foto-01-min.jpg',
         size: 300,
-        source: 'product-2.html'
+        source:'product-2.html'
     },
     {
         id:3,
@@ -27,7 +27,7 @@ const cardsData = [
         producer: 'ЗИК Все',
         img: 'images/knifes/three/nozh-lesnoy-elmax-stabilizirovannaya-karelskaya-bereza-alyuminiy-foto-01-min.jpg',
         size: 340,
-        source: 'product-3.html'
+        source:'product-3.html'
     },
     {
         id:4,
@@ -37,7 +37,7 @@ const cardsData = [
         producer: 'Булат Сергея Баранова Все',
         img: 'images/knifes/seven/nozh-spec-dn-100h13m-berezovyy-kap-latun-zolochenie-klinka-gardy-i-tylnika-foto-01-min.jpg',
         size: 290,
-        source: 'product-4.html'
+        source:'product-4.html'
     },
     {
         id:5,
@@ -47,7 +47,7 @@ const cardsData = [
         producer: 'Булат Сергея Баранова Все',
         img: 'images/knifes/eight/nozh-udachnaya-ohota-zolochenie-foto-02.jpg',
         size: 290,
-        source: 'product-5.html'
+        source:'product-5.html'
     },
     {
         id:6,
@@ -57,7 +57,7 @@ const cardsData = [
         producer: 'АиР Все',
         img:'images/knifes/five/nozh-ermak-40h10s2m-nabornaya-kozha-tekstolit-foto-01-min.jpg',
         size: 340,
-        source: 'product-6.html'
+        source:'product-6.html'
     },
     {
         id:7,
@@ -67,7 +67,7 @@ const cardsData = [
         producer: 'АиР Все',
         img: 'images/knifes/four/nozh-duplet-2-s-formovannymi-nozhnami-damask-zdi-1016-stabilizirovannaya-karelskaya-bereza-latun-foto-01-min.jpg',
         size: 335,
-        source: 'product-7.html'
+        source:'product-7.html'
     },
     {
         id:8,
@@ -77,6 +77,6 @@ const cardsData = [
         producer: 'АиР Все',
         img: 'images/knifes/six/nozh-dombay-40h10s2m-oreh-tekstolit-foto-01-min.jpg',
         size: 300,
-        source: 'product-8.html'
+        source:'product-8.html'
     }
 ]
